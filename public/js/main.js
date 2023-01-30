0c72c168cda4758f5b7e71f346622c1992e3be32
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-30 16:06:39 +0100

    mode sombre

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,17 @@
 function toggleModal() {
     document.getElementById("overlay").classList.toggle('active');
 }
+
+//Bouton mode clair et sombre
+let toggle_btn = document.getElementById('theme-btn');
+let body = document.getElementsByTagName('body')[0];
+let dark_theme_class = 'dark';
+
+toggle_btn.addEventListener('click', function() {
+if (body.classList.contains(dark_theme_class)) {
+body.classList.remove(dark_theme_class);
+}
+else {
+body.classList.add(dark_theme_class);
+}
+});
