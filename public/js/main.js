2568142ca4736b0a085d2ffadfd30e922b880604
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-31 15:22:40 +0100

    section 5 debut et html

--- public/js/main.js
+++ public/js/main.js
@@ -16,3 +16,125 @@ else {
 body.classList.add(dark_theme_class);
 }
 });
+
+
+//Espace JS
+//les produits
+let products = {
+    data: [
+    {
+        productName: "fazfaez",
+        category: "Starters",
+        price: "30",
+        image: "./public/img/menu/menu-item-1.png",
+},
+    {
+        productName: "fazefaze",
+        category: "Breakfast",
+        price: "49",
+        image: "./public/img/menu/menu-item-2.png",
+},
+    {
+        productName: "afzefezaazef",
+        category: "Dinner",
+        price: "99",
+        image: "./public/img/menu/menu-item-6.png",
+},
+    {
+        productName: "fazefaez",
+        category: "Starters",
+        price: "29",
+        image: "./public/img/menu/menu-item-4.png",
+},
+    {
+        productName: "fazfzaefaze",
+        category: "Lunch",
+        price: "129",
+        image: "./public/img/menu/menu-item-5.png",
+},
+    {
+        productName: "azfeafze",
+        category: "Breakfast",
+        price: "89",
+        image: "./public/img/menu/menu-item-1.png",
+},
+    {
+        productName: "afzefaez",
+        category: "Lunch",
+        price: "189",
+        image: "./public/img/menu/menu-item-4.png",
+},
+    {
+        productName: "fazefaez",
+        category: "Breakfast",
+        price: "49",
+        image: "./public/img/menu/menu-item-2.png",
+        },
+    ],
+};
+for (let i of products.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //La carte doit avoir une categorie
+    card.classList.add("card", i.category);
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //product name
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    //prix
+    let price = document.createElement("h6");
+    price.innerText = "$" + i.price;
+    container.appendChild(price);
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+}
+  //parameter passed from button (Parameter same as category)
+function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+} else {
+        button.classList.remove("active");
+}
+});
+
+//select all cards
+let elements = document.querySelectorAll(".card");
+//loop through all cards
+elements.forEach((element) => {
+//display all cards on 'all' button click
+    if (value == "all") {
+        element.classList.remove("hide");
+    } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+        element.classList.remove("hide");
+    } else {
+          //hide other elements
+        element.classList.add("hide");
+    }
+        }
+    });
+}
+
+
+//Initially display all products
+window.onload = () => {
+    ("Starters");
+};
